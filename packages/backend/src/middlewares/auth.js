7e587e4b240e92b11ef258810cb0aa0984385449
NomikuH8 <sofaco.checkin@gmail.com>
2025-05-15 19:49:43 -0300

    Fixed authorization token

--- packages/backend/src/middlewares/auth.js
+++ packages/backend/src/middlewares/auth.js
@@ -10,7 +10,7 @@ const JWT_SECRET = "segredo-teste";
  * @param {e.NextFunction} next Next function
  */
 export function authMiddleware(req, res, next) {
-  const token = req.cookies.token ?? req.headers["Authorization"];
+  const token = req.cookies.token ?? req.headers["authorization"];
 
   if (!token) {
     return res.status(401).json({
